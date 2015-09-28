bb9e549b86b1a403947c3c156a6d1e4f95749e5d
Barbrady <pi@piserver.(none)>
2015-09-28 13:06:23 +0200

    Actualización.

--- src/tamanioentero.c
+++ src/tamanioentero.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+void swap (int *px, int *py)
+{
+    int temp;
+    temp = *px;
+    *px = *py;
+    *py = temp;
+}
+
+
+
 int main()
 {
     int i = sizeof(int);
+    int c = sizeof(char);
+    int a,b;
+    a=10;
+    b=20;
+
+    printf("El tamaño de un char es %d.\n",c);
     printf("El tamaÃo de un entero es %d.\n",i);
+
+    swap(&a,&b);
+    printf("La variable a era 10 y b era 20, ahora a es %d y b es %d.\n",a,b);
 }
