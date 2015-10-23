198db7492c02f6cc0194ba0a0b8d941009f5c6c1
barbrady <oficial.barbrady@gmail.com>
2015-10-23 07:40:58 +0200

    GTK

--- src/ejemploGTK.c
+++ src/ejemploGTK.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
+#include <gtk-2.0/gtk/gtk.h>
+
 
 int main (int argc, char *argv[])  {
 	GtkWidget *ventana;
 	GtkWidget *boton;
-	
-		
-	gtk_init (&argc, &argv);
-	
-	ventana = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	boton = gtk_button_new_with_label("Botón.");
-	gtk_container_add ((GtkContainer *) ventana, boton);
-	gtk_widget_show (ventana) ;
+
+	gtk_init(&argc, &argv);
+
+	ventana = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	boton = gtk_button_new_with_label ("Botón");
+	gtk_container_add (GTK_CONTAINER (ventana), boton);
+	gtk_widget_show (ventana);
 	gtk_widget_show (boton);
+
 	gtk_main();
+	return 0;
 }
