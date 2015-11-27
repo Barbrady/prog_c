18082deb92952131a0f7b632d077680aaa95b358
Oficial Barbrady <oficial.barbrady@gmail.com>
2015-11-27 08:00:50 +0100

    Actualización añadido archivo de pruebas con fork.

---
+++ src/pruebasfork.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main ()  {
+	int pid;
+
+	printf("Vamos a probar el creado de procesos con fork.\n");
+
+	pid = fork();
+
+	if(pid<0)  {
+		printf("Error en la creación del fork.\n");
+	}
+	if(pid==0) {  
+		printf("Este es el proceso hijo con pid=%d.\n",pid);
+		execl("/bin/ls","ls","-a",NULL);
+		exit(1);
+	}
+	printf("Este es el proceso padre con pid=%d.\n",pid);
+	
+
+}
