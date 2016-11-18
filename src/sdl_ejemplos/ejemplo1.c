bb14cd8857c351f8b6680d3d484e8f6ac5c80c56
Oficial Barbrady <barbrady@a53b>
2016-11-18 20:03:58 +0100

    Actualización SDL.

---
+++ src/sdl_ejemplos/ejemplo1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL2/SDL_image.h>
+
+int main()  {
+	SDL_Surface* pantalla;
+	SDL_Surface* imagen;
+	SDL_Event evento;
+	SDL_Rect *rectangulo;
+	//pantalla = SDL_Init()
+}
